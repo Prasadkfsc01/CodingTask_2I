a3b95e8a2303f51f2cc1c292ee0e1e776aa170e4
Prasad Kotamsetti <98956122+Prasadkfsc01@users.noreply.github.com>
2023-09-07 09:10:40 +0100

    Update RemoveDuplicates.js

--- RemoveDuplicates.js
+++ RemoveDuplicates.js
@@ -1,19 +1,23 @@
 
+// Assuming the list of numbers
+const numbers = [5, 2, 10, 8, 2, 1, 5, 8, 3, 10];
+
+// Function to remove the duplicates 
 function removeDuplicatesSort(numbers) {
 
-    // removal of duplicates from the provided numbers in function parameter
+    // removal of duplicates from the provided numbers in function parameter to store in uniqueNumbers
     const uniqueNumbers = [...new Set(numbers)];
 
     // Sorting numbers in descending order
     uniqueNumbers.sort((a, b) => b - a);
+
+    // Returning uniqueNumbers
     return uniqueNumbers;
 }
 
-// Assuming the list of numbers
-const numbers = [5, 2, 10, 8, 2, 1, 5, 8, 3, 10];
 
-// storing the unique numbers in sortedNumber variable 
+// Run removeDuplicatesSort function & storing uniqueNumbers value in sortedNumbers variable 
 const sortedNumbers = removeDuplicatesSort(numbers);
 
-// Printing the unique numbers
+// Printing the sortedNumbers which contains unique numbers sorted in decending order
 console.log(sortedNumbers);
